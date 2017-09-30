358e8dcd8f84e3f5be87690884e6e7a1461a8e20
DigitalSonic <digitalsonic.dxf@gmail.com>
2017-09-30 20:19:19 +0800

    add the first simple microservice

---
+++ section-01/order-service/src/main/java/microbucks/orderservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package microbucks.orderservice.repository;
+
+import microbucks.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomer(String customer);
+}
---
+++ section-01/order-service/src/main/java/microbucks/orderservice/runner/DataInitializerRunner.java
@@ -0,0 +1,28 @@
+package microbucks.orderservice.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import microbucks.orderservice.model.Order;
+import microbucks.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class DataInitializerRunner implements CommandLineRunner {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Arrays.asList("Li Lei", "Han Meimei").forEach(x -> {
+            Order order = Order.builder().customer(x).barista("Master")
+                    .createTime(new Date()).modifyTime(new Date()).build();
+            orderRepository.save(order);
+            log.info("Saving Order[{}] to database.", order);
+        });
+    }
+}
