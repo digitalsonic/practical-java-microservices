625f1e076a54bb625be9712155c6e842ab17cb5d
DigitalSonic <digitalsonic.dxf@gmail.com>
2017-10-01 23:53:18 +0800

    移除未用到的import

--- section-01/order-service/src/main/java/microbucks/orderservice/repository/OrderRepository.java
+++ section-01/order-service/src/main/java/microbucks/orderservice/repository/OrderRepository.java
@@ -3,7 +3,5 @@ package microbucks.orderservice.repository;
 import microbucks.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
